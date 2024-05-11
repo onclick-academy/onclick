ad9656601285c36d8a194de16710fa7c7abba55e
husamahmud <husamahmud@gmail.com>
2024-05-11 03:15:04 +0300

    feat(front): Implement `Landing` and `NavBar`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import Hero from '@/components/Hero/Hero'
-import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='min-h-screen'>
       <Hero />
     </main>
   )
