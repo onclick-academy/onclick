f9cce35ba0a8441170ed510dbf612ef90037510f
husamahmud <husamahmud@gmail.com>
2024-05-28 02:29:48 +0300

    fix(front): Fix

--- src/components/Hero/Header.tsx
+++ src/components/Hero/Header.tsx
@@ -12,7 +12,7 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger
+  DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import {
   CommandDialog,
@@ -20,22 +20,25 @@ import {
   CommandGroup,
   CommandInput,
   CommandItem,
-  CommandList
+  CommandList,
 } from '@/components/ui/command'
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
 import { NavMenu } from '@/components/Hero/NavMenu'
-import getData from '@/utilities/getUserData'
+// import getData from '@/utilities/getUserData'
 import Btn from '@/constants/Btn'
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle
+  navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu'
-import * as React from 'react'
 
 export default function Header() {
   const [open, setOpen] = useState<boolean>(false)
@@ -44,20 +47,20 @@ export default function Header() {
   const links = [
     {
       title: 'Home',
-      href: '#'
+      href: '#',
     },
     {
       title: 'Courses',
-      href: '#courses'
+      href: '#courses',
     },
     {
       title: 'News',
-      href: '#news'
+      href: '#news',
     },
     {
       title: 'Events',
-      href: '#events'
-    }
+      href: '#events',
+    },
   ]
 
   useEffect(() => {
@@ -86,94 +89,97 @@ export default function Header() {
 
   return (
     <>
-      <header className='container bg-transparent py-6 flex justify-between items-center z-10 relative'>
-        <div className='flex items-center gap-4'>
-          <Link href='#'>OnClick</Link>
-          <div className='hidden md:block'>
-            <NavMenu />
-          </div>
-        </div>
-
-        <div className='hidden md:block'>
-          <NavigationMenu>
-            <NavigationMenuList>
-              {links.map((link, index) => (
-                <NavigationMenuItem key={index}>
-                  <Link href={link.href} passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>{link.title}</NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              ))}
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
-
-        <div className='flex items-center gap-5'>
-          <button onClick={togleOpen}>
-            <Search width={30} />
-          </button>
-
-          {userData ? (
-            <div className='gap-2 hidden md:flex'>
-              <Btn className='px-5 py-3'>
-                <Link href='#'>Login</Link>
-              </Btn>
-              <Btn className='px-5 py-3'>
-                <Link href='#'>Signup</Link>
-              </Btn>
+      <header className="w-screen py-6 z-10">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="flex items-center gap-4">
+            <Link href="#">OnClick</Link>
+            <div className="hidden md:block">
+              <NavMenu />
             </div>
-          ) : (
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <Avatar>
-                  <AvatarImage src='https://github.com/shadcn.png' className='z-10' />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Cart</DropdownMenuItem>
-                <DropdownMenuItem>Wishlist</DropdownMenuItem>
-                <DropdownMenuItem>Logout</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          )}
+          </div>
 
-          {/** Navmenu in small screens */}
-          <div className='flex md:hidden'>
-            <Sheet>
-              <SheetTrigger>
-                <IoIosMenu size={30} />
-              </SheetTrigger>
-              <SheetContent>
-                <SheetHeader>
-                  <SheetTitle>
-                    <Link href='#'>My Account</Link>
-                  </SheetTitle>
-                  <SheetTitle>
-                    <Link href='#'>Cart</Link>
-                  </SheetTitle>
-                  <SheetTitle>
-                    <Link href='#'>Wishlist</Link>
-                  </SheetTitle>
-                  <SheetTitle>
-                    <Link href='#'>Logout</Link>
-                  </SheetTitle>
-                </SheetHeader>
-              </SheetContent>
-            </Sheet>
+          <div className="hidden md:block">
+            <NavigationMenu>
+              <NavigationMenuList>
+                {links.map((link, index) => (
+                  <NavigationMenuItem key={index}>
+                    <Link href={link.href} passHref>
+                      <NavigationMenuLink className={navigationMenuTriggerStyle()}>{link.title}</NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                ))}
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+
+          <div className="flex items-center gap-5">
+            <button onClick={togleOpen}>
+              <Search width={30} />
+            </button>
+
+            {userData ? (
+              <div className="gap-2 hidden md:flex">
+                <Btn className="px-5 py-3">
+                  <Link href="#">Login</Link>
+                </Btn>
+                <Btn className="px-5 py-3">
+                  <Link href="#">Signup</Link>
+                </Btn>
+              </div>
+            ) : (
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png"
+                                 className="z-10" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Cart</DropdownMenuItem>
+                  <DropdownMenuItem>Wishlist</DropdownMenuItem>
+                  <DropdownMenuItem>Logout</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
+
+            {/** Navmenu in small screens */}
+            <div className="flex md:hidden">
+              <Sheet>
+                <SheetTrigger>
+                  <IoIosMenu size={30} />
+                </SheetTrigger>
+                <SheetContent>
+                  <SheetHeader>
+                    <SheetTitle>
+                      <Link href="#">My Account</Link>
+                    </SheetTitle>
+                    <SheetTitle>
+                      <Link href="#">Cart</Link>
+                    </SheetTitle>
+                    <SheetTitle>
+                      <Link href="#">Wishlist</Link>
+                    </SheetTitle>
+                    <SheetTitle>
+                      <Link href="#">Logout</Link>
+                    </SheetTitle>
+                  </SheetHeader>
+                </SheetContent>
+              </Sheet>
+            </div>
           </div>
         </div>
       </header>
 
-      <div className='max-w-xl h-fit absolute left-1/2 transform -translate-x-1/2 z-10'>
+      <div className="max-w-xl h-fit absolute left-1/2 transform -translate-x-1/2 z-10">
         <CommandDialog open={open} onOpenChange={setOpen}>
-          <CommandInput placeholder='Type a command or search...' />
+          <CommandInput placeholder="Type a command or search..." />
           <CommandList>
             <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup heading='Suggestions'>
+            <CommandGroup heading="Suggestions">
               <CommandItem>Calendar</CommandItem>
               <CommandItem>Search Emoji</CommandItem>
               <CommandItem>Calculator</CommandItem>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,21 +2,55 @@ import { Badge } from 'lucide-react'
 import Image from 'next/image'
 
 import Stars from '@/constants/Stars'
-import HeroBanner from './hero-img.png'
-import Shape1 from './shape-1.png'
-import Shape2 from './shape-2.png'
-import Shape3 from './shape-3.png'
-// import Header from '@/components/Hero/Header'
-import Landing from '@/components/Hero/Landing'
-import './Hero.css'
+import HeroBanner from '../../../public/images/hero-img.png'
+import Shape1 from '../../../public/images/shape-1.png'
+import Shape2 from '../../../public/images/shape-2.png'
+import Shape3 from '../../../public/images/shape-3.png'
 
 export default function Hero() {
   /* TODO: Add floating elements*/
 
   return (
-    <div className="min-h-screen px-14 bg-hero">
-      {/*<Header />*/}
-      <Landing />
+    <div id="#"
+         className="h-auto px-14">
+      <div className="absolute left-1/2 transform z-0 -translate-x-1/2 top-0">
+        <Image src={Shape3} width={500} height={500} alt="shape1" />
+      </div>
+      <div className="absolute top-[40%] z-0">
+        <Image src={Shape1}
+               alt="shape1"
+               width={600}
+               height={600}
+               className="z-0" />
+      </div>
+      <div className="hidden lg:block absolute right-0 z-0">
+        <Image src={Shape2} alt="shape2" className="z-0" />
+      </div>
+      <div className="container mx-auto flex flex-col lg:flex-row gap-8 lg:gap-0 items-center py-20">
+        <div className="flex-1 order-2 lg:order-1">
+          <div className="flex gap-4 lg:gap-16 flex-col lg:flex-row pb-12">
+            <div className="flex flex-col items-center md:items-start gap-3">
+              <Stars fill={4} half={1} size={24} color="#FF8F3C" />
+              <p className="font-semibold ">12500+ TRUST STUDENTS</p>
+            </div>
+
+            <div className="flex flex-col items-center md:items-start gap-3">
+              <Badge fill="#FF8F3C"
+                     stroke="#FF8F3C"
+                     className="mx-auto md:mr-0 md:ml-0" />
+              <p className="font-semibold">OnClick ELITE AUTHOR</p>
+            </div>
+          </div>
+
+          <h1 className="mb-12 text-center lg:text-left text-3xl lg:text-7xl font-semibold">
+            Have your dream site in minutes for <SlidingText />
+          </h1>
+        </div>
+
+        <div className="flex-1 flex justify-center lg:order-1 z-0">
+          <Image src={HeroBanner} alt="Hero banner section" />
+        </div>
+      </div>
     </div>
   )
 }
